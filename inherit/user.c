6af9e4a07b0cad28c7be3764a9f4b83d438a9ba6
xiaolinproton <xiaolin.h@protonmail.com>
2024-03-22 23:16:21 +1100

    save

--- inherit/user.c
+++ inherit/user.c
@@ -1,1 +1,22 @@
 inherit __DIR__ "user6";
+
+
+void heart_beat()
+{
+    int hp,power,food;
+
+    hp = to_int(query("hp"));
+    power = to_int(query("power"));
+    food = to_int(query("food"));
+
+    if(food>20 && hp<100)
+    {
+        hp = hp + to_int(log2(power));
+        food = food - 1;
+    }
+    if(hp>100)
+        hp = 100;
+    set("food", food);
+    set("hp", hp);
+    save();
+}
--- inherit/user2.c
+++ inherit/user2.c
@@ -87,23 +87,3 @@ mixed process_input(string verb)
 
     return verb;
 }
-
-void heart_beat()
-{
-    int hp,power,food;
-
-    hp = to_int(query("hp"));
-    power = to_int(query("power"));
-    food = to_int(query("food"));
-
-    if(food>20 && hp<100)
-    {
-        hp = hp + to_int(log2(power));
-        food = food - 1;
-    }
-    if(hp>100)
-        hp = 100;
-    set("food", food);
-    set("hp", hp);
-
-}
