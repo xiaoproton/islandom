79f370ad71fa796f6fbc28319db0452b49cac946
xiaolinproton <xiaolin.h@protonmail.com>
2024-03-23 01:03:19 +1100

    npc

--- world/world/npc/npc.c
+++ world/world/npc/npc.c
@@ -13,8 +13,9 @@ void create()
         set("power", power);
         set("hp", 100);
         set("food", random(20)+80);
-        set("def", 3 * lv + random(30));
-        set("agi", 2 * lv + random(20));
+        set("size",random(3));
+        set("power", random(10));
+        set("unit", "");
         set("colour", random(3) ? (random(3) ? "b" : "y") : "p");
         set_heart_beat(1);
     }
