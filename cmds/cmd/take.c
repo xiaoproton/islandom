e633946c117361718d15edf7d18699b7045efdf8
xiaolinproton <xiaolin.h@protonmail.com>
2024-03-23 15:58:49 +1100

    debug

--- cmds/cmd/take.c
+++ cmds/cmd/take.c
@@ -4,15 +4,21 @@
 int main(object me, string arg)
 {
     mixed obj;
+    mixed ob_list;
     if(!arg||arg=="")
     {
         notify_fail("What do you want to pick up?");
     }
     debug_message("arg = "+arg);
-    obj=load_object(arg);
+    ob_list=environment(e)->query("objects");
+    foreach(mixed ob in ob_list)
+    {
+        debug_message(sprintf("obj = %s",ob));
+        debug_message(sprintf("obj = %s",ob->geteuid()));
+    }
     if(!obj)
     {
-        debug_message("obj = %O"+obj);
+        debug_message(sprintf("obj = %O",obj));
         me->make_inventory(obj);
         debug_message("make_inventory done");
         write("You have picked up "+obj->short()+" and placed into your inventory.");
