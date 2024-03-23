d0c0f4b991aad889a99dbbbd13a7dede92b8fedf
xiaolinproton <xiaolin.h@protonmail.com>
2024-03-23 23:19:14 +1100

    try

--- cmds/cmd/take.c
+++ cmds/cmd/take.c
@@ -26,7 +26,7 @@ int main(object me, string arg)
                 if(obj->can_be_pickedup(power))
                 {
                     obj->move_object(VOID_OB);
-                    newobj=me->make_inventory(obj->file_name());
+                    newobj=me->make_inventory(obj);
                     debug_message(sprintf("newobj = %O",newobj));
                     write(GRN+"You have picked up "+obj->short()+" and placed into your inventory.\n"+NOR);
                     obj->destruct(obj);
