e10dcceb202ba69dd796a7b28308fd86fea870ef
xiaolinproton <xiaolin.h@protonmail.com>
2024-03-23 21:50:03 +1100

    fix

--- world/world/npc/npc_ant.c
+++ world/world/npc/npc_ant.c
@@ -33,7 +33,7 @@ void heart_beat()
     {
         say("[NPC Ant]: working harder...\n");
 
-        fd = clone_object(__DIR__ "../item/food") ;
+        fd = clone_object("/world/world/item/food") ;
         debug_message(sprintf("created %O",fd));
         fd->move_object(environment(this_object()));
         debug_message(sprintf("fd env %O",environment(fd)));
