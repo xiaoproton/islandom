98e262f4c93b8bcd77b4bb93f3034f2790397139
xiaolinproton <xiaolin.h@protonmail.com>
2024-03-23 16:25:36 +1100

    debug

--- world/world/item/food.c
+++ world/world/item/food.c
@@ -4,7 +4,7 @@ inherit OBJECT;
 void create()
 {
     set("name", "Food");
-    set("long","A piece of food that can be eaten ("+GRN+"eat"+NOR+") to increase your Nutrition.\n"
+    set("long","Food that can be eaten ("+GRN+"eat"+NOR+") to increase your Nutrition.\n"
                 +"Or, it can be fed ("+GRN+"feed"+NOR+") to others.\n");
-    set("unit", " piece of ");
+    set("unit", " ");
 }
